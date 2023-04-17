29a2dedd3ec8fd35b1b721c4c9e9afef2c640767
LuPeBreak <lfbmrj15@gmail.com>
2023-04-17 14:33:54 -0300

    Inicio do projeto - criado thema - estilos globais - componente header iniciado - pagina Transactions iniciada

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+
+export function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello World</h1>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
+}
