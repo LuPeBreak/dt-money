406be5773e77331f8dea20003417588785d7c03f
LuPeBreak <lfbmrj15@gmail.com>
2023-04-19 10:47:36 -0300

    Criado Contexto de Transactions

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
 import { Transactions } from "./pages/Transactions";
+import { TransactionsProvider } from "./contexts/TransactionsContext";
 
 export function App() {
   const [count, setCount] = useState(0);
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Transactions/>
+      <TransactionsProvider>
+        <Transactions />
+      </TransactionsProvider>
       <GlobalStyle />
     </ThemeProvider>
   );
